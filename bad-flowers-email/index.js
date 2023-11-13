a628ed1b443a5ba569152f82e8af418a4935fa40
rosveluz <112733712+rosveluz@users.noreply.github.com>
2023-11-14 04:23:48 +0800

    FULLY WORKING MOTHERFUCKER

--- bad-flowers-email/index.js
+++ bad-flowers-email/index.js
@@ -7,8 +7,8 @@
  * See a full list of supported triggers at https://firebase.google.com/docs/functions
  */
 
-// const {onRequest} = require("firebase-functions/v2/https");
-// const logger = require("firebase-functions/logger");
+const {onRequest} = require("firebase-functions/v2/https");
+const logger = require("firebase-functions/logger");
 
 // Create and deploy your first functions
 // https://firebase.google.com/docs/functions/get-started
@@ -17,34 +17,3 @@
 //   logger.info("Hello logs!", {structuredData: true});
 //   response.send("Hello from Firebase!");
 // });
-
-
-const functions = require('firebase-functions');
-const sgMail = require('@sendgrid/mail');
-
-// Set the SendGrid API key from Firebase configuration
-sgMail.setApiKey(functions.config().sendgrid.key);
-
-exports.sendEmail = functions.firestore
-    .document('userImages/{documentId}')
-    .onCreate(async (snap, context) => {
-        const newValue = snap.data();
-
-        const msg = {
-            to: newValue.email, // recipient
-            from: 'your-email@example.com', // verified sender
-            subject: 'Sending with SendGrid is Fun',
-            text: 'and easy to do anywhere, even with Firebase',
-            html: '<strong>and easy to do anywhere, even with Firebase</strong>',
-        };
-
-        try {
-            await sgMail.send(msg);
-            console.log('Email sent');
-        } catch (error) {
-            console.error('Error sending email', error);
-            if (error.response) {
-                console.error(error.response.body);
-            }
-        }
-    });
--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,3 @@
-/* index.js */
 const functions = require("firebase-functions");
 const nodemailer = require("nodemailer");
 const admin = require("firebase-admin");
@@ -15,61 +14,60 @@ const storage = new Storage();
 
 // Email transporter configuration using Gmail.
 const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: functions.config().gmail.email,
-    pass: functions.config().gmail.password,
-  },
+    service: "gmail",
+    auth: {
+        user: functions.config().gmail.email,
+        pass: functions.config().gmail.password,
+    },
 });
 
-exports.sendEmailWithAttachment = functions.firestore
-    .document("captured-flowers/{docId}")
+exports.sendEmailWithAttachment = functions.region('asia-southeast1').firestore
+    .document("userImages/{docId}")
     .onCreate(async (snap, context) => {
-      const {email, imageURL} = snap.data();
+        const documentData = snap.data();
+        const email = documentData.email; // make sure 'email' field exists in the document
+        const image = documentData.image; // use 'image' instead of 'images' or 'imageURL'
 
-      // Check if imageURL is a path in the bucket or a base64 string
-      let filePath;
-      let attachments;
+        // Check if 'image' is a path in the bucket or a base64 string
+        let filePath;
+        let attachments;
 
-      if (imageURL.startsWith("gs://") || imageURL.startsWith("https://")) {
-        // If it's a path, download the image to a temporary directory.
-        filePath = path.join(os.tmpdir(), "image.jpg");
-        const storageBucket = storage.bucket("bad-flowers.appspot.com");
-        const fileName = imageURL.split("/").pop();
-        const file = storageBucket.file(fileName);
+        if (image && image.startsWith("data:image/")) {
+            // Determine the image format (png, jpg, etc.)
+            const matches = image.match(/^data:(image\/\w+);base64,/);
+            const extension = matches[1].split('/')[1];
+            const base64Data = image.replace(/^data:image\/\w+;base64,/, "");
 
-        await file.download({destination: filePath});
-        attachments = [{filename: "image.jpg", path: filePath}];
-      } else if (imageURL.startsWith("data:image/")) {
-        // If it's a base64 string, decode it and attach directly.
-        const base64Data = imageURL.replace(/^data:image\/\w+;base64,/, "");
-        attachments = [{
-          filename: "image.jpg",
-          content: Buffer.from(base64Data, "base64"),
-          encoding: "base64",
-        }];
-      } else {
-        throw new Error("Invalid image URL format");
-      }
-
-      const mailOptions = {
-        from: "cnvs team <connect@cnvs.zip>",
-        to: email,
-        subject: "Bad Flowers",
-        text: "Please find the attached image.",
-        attachments: attachments,
-      };
-
-      try {
-        // Send the email with the attached image.
-        await transporter.sendMail(mailOptions);
-        console.log("Email sent successfully");
-      } catch (error) {
-        console.error("Error sending email:", error);
-      } finally {
-        // Cleanup the temp file if it was created.
-        if (filePath) {
-          fs.unlinkSync(filePath);
+            // Use the correct file extension based on the image format
+            const filename = `image.${extension}`;
+            attachments = [{
+                filename: filename,
+                content: Buffer.from(base64Data, "base64"),
+                encoding: "base64",
+                contentType: matches[1], // This sets the correct content type
+            }];
+        } else {
+            console.error("Invalid or missing image field");
+            return;
         }
-      }
+
+        const mailOptions = {
+            from: "cnvs team <connect@cnvs.zip>",
+            to: email,
+            subject: "Your Image",
+            text: "Please find the attached image.",
+            attachments: attachments,
+        };
+
+        try {
+            // Send the email with the attached image.
+            await transporter.sendMail(mailOptions);
+            console.log("Email sent successfully to:", email);
+        } catch (error) {
+            console.error("Error sending email:", error);
+        } finally {
+            // No need to clean up the temp file since we're not creating one
+        }
     });
+
+
